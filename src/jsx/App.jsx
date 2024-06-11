e4840832fab7cfeeeac07e0114025f1b84f5a9d9
Jacob DeBenedetto <deben3@gmail.com>
2024-06-11 08:59:47 -0700

    Add PDF link

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const count = createRef();
   const canvas = createRef();
   const instructions = createRef();
-  const tutorial = createRef();
+  const hint = createRef();
   const game = window.game = new Game();
   
   // Update canvas render
@@ -23,11 +23,15 @@ function App() {
   // Increment count
   function incrementCount() {
     count.current.innerHTML++;
-    tutorial.current.style.display = 'none';
+    hint.current.style.display = 'none';
   }
 
   function toggleWireframe(e) {
     game.gameManager.worldManager.debugger.toggle();
+  }
+
+  function openPDF(e) {
+    window.open('pdf/resume-jacob-debenedetto-2024.pdf', '_blank');
   }
 
   // Return game UI
@@ -36,11 +40,17 @@ function App() {
       <canvas ref={canvas} />
       <div className="ui">
         <div className="count" ref={count}>0</div>
-        <div className="tutorial material-symbols-rounded" ref={tutorial}>arrow_selector_tool</div>
-        <div className="toggle">
-          <input type="checkbox" id="wireframe" onChange={toggleWireframe}></input>
-          <label htmlFor="wireframe"><span>DEBUG</span></label>
+        <div className="top-right">
+          <div className="toggle">
+            <input type="checkbox" id="wireframe" onChange={toggleWireframe}></input>
+            <label htmlFor="wireframe">DEBUG</label>
+          </div>
+          <div className="toggle">
+            <input type="checkbox" id="pdf" onChange={openPDF}></input>
+            <label htmlFor="pdf"><span>PDF</span></label>
+          </div>
         </div>
+        <div className="hint material-symbols-rounded" ref={hint}>arrow_selector_tool</div>
         <div className="instructions" ref={instructions}>Tap to toss Jacob's resume</div>
       </div>
     </>
